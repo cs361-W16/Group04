f0a3a12342970183318bea657dfb6de5fc2f8096
joshtrav11 <mattesoj@oregonstate.edu>
2016-01-22 16:09:31 -0800

    First-commit
    Added card class, deck class, with testing functionality.

---
+++ Assignment1/src/main/java/model/Card.java
@@ -0,0 +1,26 @@
+package model;
+
+/**
+ * Created by joshmatteson on 1/22/16.
+ */
+public class Card {
+
+    int rank;
+    String suit;
+
+    public Card(int r, int s) {
+
+        rank = r;
+        if (s == 0) {
+            suit = "spades";
+        } else if (s == 1) {
+            suit = "hearts";
+        } else if (s == 2) {
+            suit = "clubs";
+        } else {
+            suit = "diamonds";
+        }
+    }
+    public int getRank() {return rank;}
+    public String getSuit() {return suit;}
+}
---
+++ Assignment1/src/main/java/model/Deck.java
@@ -0,0 +1,33 @@
+package model;
+
+/**
+ * Created by joshmatteson on 1/22/16.
+ */
+public class Deck {
+    Card[] cards;
+    int top;
+
+    public Deck() {
+        cards = new Card[52];
+        top = 0;
+    }
+
+    public void makeDeck() {
+        for(int i=0; i<13; i++) {
+            for(int j=0; j<4; j++) {
+                cards[i*4+j] = new Card(i,j);
+            }
+        }
+    }
+
+    public Card member(int idx) {return cards[idx];}
+
+    public Card dealTop() {
+        if(top <52) {
+            Card c = cards[top];
+            top += 1;
+            return c;
+        }
+        return null;
+    }
+}
